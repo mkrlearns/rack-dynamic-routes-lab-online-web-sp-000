f8c103ec386af9050520f5e4e5100c06bf8258c6
ryan m <contact@ryanmeek.com>
2020-02-17 13:46:45 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      name = req.path.split("/items/").last
-      item = @@items.find { |i| i.name == name }
+      item = @@items.find { |i| i.name == req.path.split("/items/").last }
       if item; resp.write item.price
       else; resp.status = 400 end
     end
